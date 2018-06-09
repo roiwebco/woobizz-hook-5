d93f755b58d539e4de476e09dcb76e5d2a895627
roiwebco <30891172+roiwebco@users.noreply.github.com>
2018-06-09 15:51:45 +0200

    Update translation

--- woobizz-hook-5.php
+++ woobizz-hook-5.php
@@ -2,10 +2,10 @@
 /*
 Plugin Name: Woobizz Hook 5 
 Plugin URI: http://woobizz.com
-Description: Add widget content before buy button on checkout page
-Author: Woobizz
+Description: Add content before the purchase button on checkout page 
+Author: WOOBIZZ.COM
 Author URI: http://woobizz.com
-Version: 1.0.0
+Version: 1.0.1
 Text Domain: woobizzhook5
 Domain Path: /lang/
 */
@@ -16,22 +16,12 @@ add_action( 'plugins_loaded', 'woobizzhook5_load_textdomain' );
 function woobizzhook5_load_textdomain() {
   load_plugin_textdomain( 'woobizzhook5', false, basename( dirname( __FILE__ ) ) . '/lang' ); 
 }
-//Check if WooCommerce is active
-if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	//echo "woocommerce is active";
-    add_action( 'widgets_init', 'woobizzhook5_widget',105);
-}else{
-	//Show message on admin
-	//echo "woocommerce is not active";
-	add_action( 'admin_notices', 'woobizzhook5_admin_notice' );
-}
 //Add Hook 5
-
 function woobizzhook5_widget() {
 	$args = array(
 				'id'            => 'woobizzhook5_id',
 				'name'          => __( 'Woobizz Hook 5', 'woobizzhook5' ),
-				'description'   => __( 'Add widget content before buy button on checkout page','woobizzhook5' ),
+				'description'   => __( 'Add content before the purchase button on checkout page','woobizzhook5' ),
 				'before_title'  => '<h2 class="widgettitle">',
 				'before_title'   => '</h2>',
 				'before_widget' => '<li id="%1$s" class="widget %2$s">',
@@ -49,11 +39,4 @@ function woobizzhook5_widget() {
 		<?php
 	}
 }
-//Hook1 Notice
-function woobizzhook5_admin_notice() {
-    ?>
-    <div class="notice notice-error is-dismissible">
-        <p><?php _e( 'Woobizz Hook 5 needs WooCommerce to work properly, If you do not use this plugin you can disable it!', 'woobizzhook5' ); ?></p>
-    </div>
-    <?php
-}
+add_action( 'widgets_init', 'woobizzhook5_widget',105);
